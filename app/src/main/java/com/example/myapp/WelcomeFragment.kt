ca0cf8c872d3a9cfb5d8203aa5deac8fa23d9a19
climentiy <clim.richy@gmail.com>
2024-04-10 03:29:39 +0400

    реализован переход от стартового фрагмента к следующему по нажатию кнопки

--- app/src/main/java/com/example/myapp/WelcomeFragment.kt
+++ app/src/main/java/com/example/myapp/WelcomeFragment.kt
@@ -4,44 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
-//private const val ARG_PARAM1 = "param1"
-//private const val ARG_PARAM2 = "param2"
 
 class WelcomFragment : Fragment() {
-    // TODO: Rename and change types of parametersTODO: Rename parameter arguments, choose names that match
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        val startButton = view.findViewById<Button>(R.id.start)
+
+        startButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_welcomFragment_to_messageFragment)
+        }
+        return view
     }
 
-    companion object {
-
-        /*
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-        WelcomFragment().apply {
-        arguments = Bundle().apply {
-        putString(ARG_PARAM1, param1)
-        putString(ARG_PARAM2, param2)
-        }
-        }
-        */
-    }
 }
-/*
-
-override fun onCreate(savedInstanceState: Bundle?) {
-super.onCreate(savedInstanceState)
-arguments?.let {
-param1 = it.getString(ARG_PARAM1)
-param2 = it.getString(ARG_PARAM2)
-}
-}
-*/
