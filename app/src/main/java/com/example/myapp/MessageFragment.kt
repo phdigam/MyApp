4696a9444cfb5f6dfd3d192b53e260588329a65a
climentiy <clim.richy@gmail.com>
2024-06-01 06:56:59 +0400

    перед включением в пакет safeargs

--- app/src/main/java/com/example/myapp/MessageFragment.kt
+++ app/src/main/java/com/example/myapp/MessageFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
 
@@ -12,8 +14,14 @@ class MessageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        val view =  inflater.inflate(R.layout.fragment_message, container, false)
+        val nextButton = view.findViewById<Button>(R.id.next)
+
+        nextButton.setOnClickListener(){
+            view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
+        }
+
+        return view
     }
 
 }
