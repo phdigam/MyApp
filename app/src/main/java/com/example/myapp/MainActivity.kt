55a27a846795cd6f702cfca421ed2b0f54f4a55d
Climentiy <kolo.484@gmail.com>
2024-06-30 00:48:43 +0400

    создать скролируемую панель приложения

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -2,9 +2,6 @@ package com.example.myapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
