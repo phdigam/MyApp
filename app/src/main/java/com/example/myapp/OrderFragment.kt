f962400d263d50db318ed2228953f52b0a671d67
phdigam <hanzhinpv@yandex.ru>
2024-07-13 08:05:51 +0400

    заменить поиск элементов view по ИД findViewById связыванием binding

--- app/src/main/java/com/example/myapp/OrderFragment.kt
+++ app/src/main/java/com/example/myapp/OrderFragment.kt
@@ -5,46 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.chip.Chip
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.example.myapp.databinding.FragmentOrderBinding
 import com.google.android.material.snackbar.Snackbar
 
 
 class OrderFragment : Fragment() {
 
+    private var _binding: FragmentOrderBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_order, container, false)
-        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
-        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+//        val view = inflater.inflate(R.layout.fragment_order, container, false)
+        _binding = FragmentOrderBinding.inflate(inflater, container, false)
+        val view = binding.root
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
 
-        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
-        fab.setOnClickListener {
-            val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
-            val pizzaType = pizzaGroup.checkedRadioButtonId //Получаем ID выбранной кнопки
+        binding.fab.setOnClickListener {
+            val pizzaType = binding.pizzaGroup.checkedRadioButtonId //Получаем ID выбранной кнопки
             if (pizzaType == -1){
                 val text = "You need to choose a pizza type"
-                Toast.makeText(activity, text, Toast.LENGTH_SHORT).show() //
+                Toast.makeText(activity, text, Toast.LENGTH_SHORT).show() // показываем всплывающее
+                    // сообщение о необходимости выбора пиццы
             } else {
                 var text = (when (pizzaType) {
                     R.id.radio_diavolo -> "Diavolo pizza"
                     else -> "Funghi pizza"
                 })
-                val parmesan = view.findViewById<Chip>(R.id.parmesan)
-                text += if (parmesan.isChecked) ", extra parmesan" else ""
-                val chilioil = view.findViewById<Chip>(R.id.chili_oil)
-                text += if (chilioil.isChecked) ", extra chili oil" else ""
-                Snackbar.make(fab, text, Snackbar.LENGTH_SHORT).show()
+                text += if (binding.parmesan.isChecked) ", extra parmesan" else ""
+                text += if (binding.chiliOil.isChecked) ", extra chili oil" else ""
+                Snackbar.make(binding.fab, text, Snackbar.LENGTH_SHORT).show()
             }
         }
 
         return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
